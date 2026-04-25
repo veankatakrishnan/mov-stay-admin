381bcc8e180501595c30ddb670c3c9ee75da8422
Veankata-Krishnan <veankatakrishnan@gmail.com>
2026-04-25 12:32:40 +0530

    d

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
           />
         </div>
         <h2 className="mt-2 text-center text-3xl font-bold tracking-tight text-gray-900">
-          Admin Portal
+          MOV Stay Admin Portal
         </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
           Sign in to access MOV Stay analytics
